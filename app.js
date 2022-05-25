e8c7d784395ef022260c86dd955c76c7934f106a
Tushar <tm2582001@gmail.com>
2022-05-25 21:53:56 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require('path');
+const ejs = require('ejs');
+const app = express();
+
+app.set('view engine','ejs');
+app.set("views", path.join(__dirname, "views"));
+
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+
+
+const port = process.env.PORT || 8080
+
+app.listen(port,()=>{
+    console.log(`Server running on ${port}`)
+})
