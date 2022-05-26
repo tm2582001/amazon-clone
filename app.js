e03defa95c17c73369e1a5577858059e4d457354
Tushar <tm2582001@gmail.com>
2022-05-26 22:30:59 +0530

    added logo and location box

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/',(req,res)=>{
-    res.render('home')
+    res.render('pages/home')
 })
 
 
